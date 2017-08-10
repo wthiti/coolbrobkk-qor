746011754ca7c6b61c4d4a91bdc5f447eaae0327
Thiti Watanasrimongkol <golf.thiti@gmail.com>
2017-08-11 00:56:25 +0700

    Add mailer

--- app/controllers/cart.go
+++ app/controllers/cart.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/mail"
 	"strconv"
 
 	"bitbucket.org/wthiti/coolbrobkk-qor/app/models"
 	"bitbucket.org/wthiti/coolbrobkk-qor/config"
 	"bitbucket.org/wthiti/coolbrobkk-qor/config/view"
+	"github.com/qor/mailer"
 )
 
 type Cart struct {
@@ -71,5 +73,42 @@ func CartDelete(w http.ResponseWriter, req *http.Request) {
 }
 
 func CartConfirmOrder(w http.ResponseWriter, req *http.Request) {
+	v := view.ViewValueMap()
+	cookieStorer := config.GetCookieStorer(w, req)
+	jsonStr, _ := cookieStorer.Get("cart")
+	cart := Cart{}
+	json.Unmarshal([]byte(jsonStr), &cart)
 
+	cart.Name = req.FormValue("name")
+	cart.Address = req.FormValue("address")
+	cart.Email = req.FormValue("email")
+	cart.Phone = req.FormValue("phone")
+
+	storeAddressInCookie(&cart, w, req)
+	createPreorderMail(&cart, w, req)
+	view.View.Execute("cart/cart_complete", v, req, w)
+}
+
+func createPreorderMail(cart *Cart, w http.ResponseWriter, req *http.Request) {
+	v := view.ViewValueMap()
+	v["cart"] = cart
+	template := mailer.Template{
+		Name:    "preorder",
+		Data:    v,
+		Request: req,
+		Writer:  w,
+	}
+	email := config.Mailer.Render(template)
+	email.TO = []mail.Address{mail.Address{Address: "thiti.wa@coolbrobkk.com"}}
+	email.From = &mail.Address{Address: "sales@coolbrobkk.com"}
+	email.Subject = "Test Preorder"
+	config.Mailer.Send(email)
+}
+
+func storeAddressInCookie(cart *Cart, w http.ResponseWriter, req *http.Request) {
+	cookieStorer := config.GetCookieStorer(w, req)
+	cookieStorer.Put("customer_name", cart.Name)
+	cookieStorer.Put("customer_address", cart.Address)
+	cookieStorer.Put("customer_email", cart.Email)
+	cookieStorer.Put("customer_phone", cart.Phone)
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"github.com/qor/mailer"
+	"github.com/qor/mailer/gomailer"
+	"gopkg.in/gomail.v2"
+)
+
+var (
+	Mailer *mailer.Mailer
+)
+
+func init() {
+	dialer := gomail.NewDialer("smtp.zoho.com", 587, "daemon@coolbrobkk.com", "iQBEW7zY#C3z")
+	sender, _ := dialer.Dial()
+
+	Mailer = mailer.New(&mailer.Config{
+		Sender: gomailer.New(&gomailer.Config{
+			Sender: sender,
+		}),
+	})
+
+}
